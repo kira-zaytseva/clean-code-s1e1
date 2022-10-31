51c57787c0c822f44fa414a4653febf0224d60aa
Kira Zaytseva <kira.zaytseva97@gmail.com>
2022-10-31 23:47:38 +0300

    refactor: add alt to images according to the rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
     deleteButtonImg.className = "img-remove";
+    deleteButtonImg.alt = "Remove icon";
 
     label.innerText=taskString;
     label.className='task-label';
